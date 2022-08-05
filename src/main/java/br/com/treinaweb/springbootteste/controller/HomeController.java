cec0acee4e7113733f2ec4a263bb976dac116228
MatheusGorga <matheusgobar@gmail.com>
2022-08-05 18:08:44 -0300

    controller

--- src/main/java/br/com/treinaweb/springbootteste/controller/HomeController.java
+++ src/main/java/br/com/treinaweb/springbootteste/controller/HomeController.java
@@ -1,8 +1,11 @@
 package br.com.treinaweb.springbootteste.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/")
@@ -12,4 +15,18 @@ public class HomeController {
   public String home(){
     return "home";
   }
+
+  @GetMapping("/modelmap")
+  public String home2(ModelMap modelMap){
+    modelMap.addAttribute("name", "Gorga");
+    return "home";
+  }
+
+  @GetMapping("/modelandview")
+  public ModelAndView home3(){
+    var modelAndView = new ModelAndView("home");
+    modelAndView.addObject("name", "Matheus");
+    return modelAndView;
+  }
+
 }
